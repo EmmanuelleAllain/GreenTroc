5e6f58aec0a7fd9a450b77b6cae12b141b9dec75
EmmanuelleAllain <e.allain13@gmail.com>
2022-07-20 09:04:24 +0200

    home

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -8,10 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ItemController extends AbstractController
 {
-    #[Route('/item', name: 'app_item')]
+    #[Route('/', name: 'app_home')]
+    public function home(): Response
+    {
+        return $this->render('home.html.twig');
+    }
+
+    #[Route('/tous-les-objets', name: 'app_item')]
     public function index(): Response
     {
         return $this->render('item/index.html.twig', [
+            'controller_name' => 'ItemController',
+        ]);
+    }
+
+    #[Route('/nouvel-objet', name: 'app_item_new')]
+    public function new(): Response
+    {
+        return $this->render('item/new.html.twig', [
             'controller_name' => 'ItemController',
         ]);
     }
